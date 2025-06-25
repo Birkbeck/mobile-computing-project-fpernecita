c418f1dbaf1a29f85565044399a96510eb714ecd
Francis Pernecita <fperne01@student.bbk.ac.uk>
2025-06-25 09:56:49 +0100

    Added a function to `RecipeViewModel` to read all recipes.
    This function uses a coroutine to fetch all recipes from the `RecipesDao` and updates the `_recipes` LiveData with the retrieved list. It also logs the retrieved recipes.

--- app/src/main/java/co/uk/bbk/culinarycompanion_francispernecita/RecipeViewModel.kt
+++ app/src/main/java/co/uk/bbk/culinarycompanion_francispernecita/RecipeViewModel.kt
@@ -12,4 +12,15 @@ class RecipeViewModel: ViewModel() {
     val recipes: LiveData<List<Recipe>> = _recipes
 
     var recipesDao = RecipesDao? = null
+
+    fun readAllRecipes() {
+        viewModelScope.launch {
+            recipesDao?.let {
+                val recipes = it.getAllRecipes()
+
+                Log.i("Recipe", recipes.toString())
+                _recipes.value = recipes
+            }
+        }
+    }
 }
