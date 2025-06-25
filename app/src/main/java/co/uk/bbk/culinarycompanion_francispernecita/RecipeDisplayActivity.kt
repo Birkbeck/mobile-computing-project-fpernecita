3303c9fd398439c2ba08294acd93001c6e6b2d79
Francis Pernecita <fperne01@student.bbk.ac.uk>
2025-06-25 13:41:23 +0100

    Made RecipeDisplayActivity extend AppCompatActivity

--- app/src/main/java/co/uk/bbk/culinarycompanion_francispernecita/RecipeDisplayActivity.kt
+++ app/src/main/java/co/uk/bbk/culinarycompanion_francispernecita/RecipeDisplayActivity.kt
@@ -8,5 +8,6 @@ import androidx.appcompat.app.AlertDialog
 import co.uk.bbk.culinarycompanion_francispernecita.databinding.ActivityRecipeDisplayBinding
 import co.uk.bbk.culinarycompanion_francispernecita.databinding.DialogConfirmDeleteBinding
 
-class RecipeDisplayActivity {
+class RecipeDisplayActivity : AppCompatActivity() {
+
 }
