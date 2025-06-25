7f70944a9116274addc1026ebe2ac822f9d551d3
Francis Pernecita <fperne01@student.bbk.ac.uk>
2025-06-25 15:55:14 +0100

    Make RecyclerView items clickable

--- app/src/main/java/co/uk/bbk/culinarycompanion_francispernecita/RecipeAdapter.kt
+++ app/src/main/java/co/uk/bbk/culinarycompanion_francispernecita/RecipeAdapter.kt
@@ -6,8 +6,9 @@ import androidx.recyclerview.widget.RecyclerView
 import co.uk.bbk.culinarycompanion_francispernecita.databinding.RecipeItemBinding
 
 class RecipeAdapter(
-    private var recipes: List<Recipe> = listOf()) :
-    RecyclerView.Adapter<RecipeAdapter.RecipeViewHolder>() {
+    private var recipes: List<Recipe> = listOf()),
+    private val onClick: (Recipe) -> Unit
+) : RecyclerView.Adapter<RecipeAdapter.RecipeViewHolder>() {
     // ViewHolder class for each recipe item
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipeViewHolder {
         val binding = RecipeItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
