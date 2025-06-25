64782ae45001290c6ede05f4be377b25028ab8cd
Francis Pernecita <fperne01@student.bbk.ac.uk>
2025-06-25 09:54:37 +0100

    Initialized LiveData and DAO for RecipeViewModel

--- app/src/main/java/co/uk/bbk/culinarycompanion_francispernecita/RecipeViewModel.kt
+++ app/src/main/java/co/uk/bbk/culinarycompanion_francispernecita/RecipeViewModel.kt
@@ -8,5 +8,8 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 
 class RecipeViewModel: ViewModel() {
-    
+    private val _recipes = MutableLiveData<ListOf<Recipe>>()
+    val recipes: LiveData<List<Recipe>> = _recipes
+
+    var recipesDao = RecipesDao? = null
 }
