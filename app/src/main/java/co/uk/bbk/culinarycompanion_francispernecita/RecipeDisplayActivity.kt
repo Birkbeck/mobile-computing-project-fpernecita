7332ae1e267a73b556be6ba081aabd5bb7a514a3
Francis Pernecita <fperne01@student.bbk.ac.uk>
2025-06-25 16:31:26 +0100

    Use correct database instance name

--- app/src/main/java/co/uk/bbk/culinarycompanion_francispernecita/RecipeDisplayActivity.kt
+++ app/src/main/java/co/uk/bbk/culinarycompanion_francispernecita/RecipeDisplayActivity.kt
@@ -20,7 +20,7 @@ class RecipeDisplayActivity : AppCompatActivity() {
 
         recipe = intent.getSerializableExtra("recipe", Recipe::class.java) ?: return finish()
 
-        viewModel.recipesDao = RecipeDatabase.getInstance(applicationContext).recipesDao()
+        viewModel.recipesDao = RecipeDatabase.getDatabase(applicationContext).recipesDao()
 
         bindRecipeDetails()
 
