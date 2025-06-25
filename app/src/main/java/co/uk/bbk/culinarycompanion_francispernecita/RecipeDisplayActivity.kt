9775d5676a42eea0d83e840d66fe660af6fce672
Francis Pernecita <fperne01@student.bbk.ac.uk>
2025-06-25 13:41:09 +0100

    add import statements for initialising basic structure for RecipeDisplayActivity

--- app/src/main/java/co/uk/bbk/culinarycompanion_francispernecita/RecipeDisplayActivity.kt
+++ app/src/main/java/co/uk/bbk/culinarycompanion_francispernecita/RecipeDisplayActivity.kt
@@ -1,4 +1,12 @@
 package co.uk.bbk.culinarycompanion_francispernecita
 
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
+import co.uk.bbk.culinarycompanion_francispernecita.databinding.ActivityRecipeDisplayBinding
+import co.uk.bbk.culinarycompanion_francispernecita.databinding.DialogConfirmDeleteBinding
+
 class RecipeDisplayActivity {
 }
