feb70ef9503e7b11d67508e0c3ba700c17590d7d
Francis Pernecita <fperne01@student.bbk.ac.uk>
2025-06-25 10:38:48 +0100

    Populate RecipesDao.kt with imports for Dao annotations

--- app/src/main/java/co/uk/bbk/culinarycompanion_francispernecita/RecipesDao.kt
+++ app/src/main/java/co/uk/bbk/culinarycompanion_francispernecita/RecipesDao.kt
@@ -1,4 +1,10 @@
 package co.uk.bbk.culinarycompanion_francispernecita
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
 class RecipesDao {
 }
