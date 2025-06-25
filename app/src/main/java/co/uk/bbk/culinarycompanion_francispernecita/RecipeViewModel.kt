927c42b82b908f30dd1ce75944fa6269b2a4be5c
Francis Pernecita <fperne01@student.bbk.ac.uk>
2025-06-25 09:53:16 +0100

    Add ViewModel inheritance to RecipeViewModel

--- app/src/main/java/co/uk/bbk/culinarycompanion_francispernecita/RecipeViewModel.kt
+++ app/src/main/java/co/uk/bbk/culinarycompanion_francispernecita/RecipeViewModel.kt
@@ -7,6 +7,6 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 
-
-class RecipeViewModel {
+class RecipeViewModel: ViewModel() {
+    
 }
