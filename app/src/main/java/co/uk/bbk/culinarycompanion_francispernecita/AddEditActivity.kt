c7b25924cb226173a25cd4b09d6f355ee9f4bdd8
Francis Pernecita <fperne01@student.bbk.ac.uk>
2025-06-25 16:27:05 +0100

    Resolve API deprecation for `getSerializableExtra` in RecipeDisplayActivity

--- app/src/main/java/co/uk/bbk/culinarycompanion_francispernecita/AddEditActivity.kt
+++ app/src/main/java/co/uk/bbk/culinarycompanion_francispernecita/AddEditActivity.kt
@@ -24,7 +24,7 @@ class AddEditActivity : AppCompatActivity() {
         binding.categorySpinner.adapter = spinnerAdapter
 
         // get DAO
-        viewModel.RecipesDao = RecipeDatabase.getInstance(applicationContext).recipesDao(
+        viewModel.recipesDao = RecipeDatabase.getInstance(applicationContext).recipesDao()
 
         // Check if recipe is an existing one
         editingRecipe = intent.getSerializableExtra("edit_recipe") as? Recipe
