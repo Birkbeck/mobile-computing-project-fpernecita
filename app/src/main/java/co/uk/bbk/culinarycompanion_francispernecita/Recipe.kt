c3f377d6dae5ffa4042bbf062459309ab44ce5e4
Francis Pernecita <fperne01@student.bbk.ac.uk>
2025-06-25 10:33:48 +0100

    Define Recipe data class as a Room entity

--- app/src/main/java/co/uk/bbk/culinarycompanion_francispernecita/Recipe.kt
+++ app/src/main/java/co/uk/bbk/culinarycompanion_francispernecita/Recipe.kt
@@ -1,4 +1,8 @@
 package co.uk.bbk.culinarycompanion_francispernecita
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+ // Recipe entity class representing the recipe table in the database
+
 class Recipe {
 }
