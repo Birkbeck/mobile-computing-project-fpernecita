8c090bb9fe323681a361654807a19a6b0012258b
Francis Pernecita <fperne01@student.bbk.ac.uk>
2025-06-25 09:50:28 +0100

    add the necessary AndroidX lifecycle imports (`LiveData`, `MutableLiveData`, `ViewModel`, `viewModelScope`) and `Log` for debugging, as well as the `kotlinx.coroutines.launch` for asynchronous operations.

--- app/src/main/java/co/uk/bbk/culinarycompanion_francispernecita/RecipeViewModel.kt
+++ app/src/main/java/co/uk/bbk/culinarycompanion_francispernecita/RecipeViewModel.kt
@@ -1,4 +1,12 @@
 package co.uk.bbk.culinarycompanion_francispernecita
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+
 class RecipeViewModel {
 }
