4ddd1fe15d90fdf62a7d1c19c21b1530a9cfb206
Francis Pernecita <fperne01@student.bbk.ac.uk>
2025-06-25 11:38:38 +0100

    Implemented functionality for adding new recipes using a dialog

--- app/src/main/java/co/uk/bbk/culinarycompanion_francispernecita/MainActivity.kt
+++ app/src/main/java/co/uk/bbk/culinarycompanion_francispernecita/MainActivity.kt
@@ -24,6 +24,7 @@ class MainActivity : AppCompatActivity() {
         val adapter = RecipeAdapter()
         binding.recipeRecyclerView.adapter = adapter
 
+        binding.addRecipeButton.setOnClickListener { showAddRecipeDialog(null) }
         }
     }
 }
