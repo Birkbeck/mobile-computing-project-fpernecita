efd982b9b98f3937a0ba52e351bb7406fefa09ea
Francis Pernecita <fperne01@student.bbk.ac.uk>
2025-06-25 11:13:26 +0100

    Define RecipesDatabase class imports

--- app/src/main/java/co/uk/bbk/culinarycompanion_francispernecita/RecipesDatabase.kt
+++ app/src/main/java/co/uk/bbk/culinarycompanion_francispernecita/RecipesDatabase.kt
@@ -1,4 +1,9 @@
 package co.uk.bbk.culinarycompanion_francispernecita
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
 class RecipesDatabase {
 }
