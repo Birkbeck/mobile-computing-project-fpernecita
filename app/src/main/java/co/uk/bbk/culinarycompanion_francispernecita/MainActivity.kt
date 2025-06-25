671310183afeed00c6761ef70d415130ecaef279
Francis Pernecita <fperne01@student.bbk.ac.uk>
2025-06-25 15:37:52 +0100

    Remove add recipe dialog from MainActivity
    The add recipe functionality has been moved to a separate activity. so it is redundant

--- app/src/main/java/co/uk/bbk/culinarycompanion_francispernecita/MainActivity.kt
+++ app/src/main/java/co/uk/bbk/culinarycompanion_francispernecita/MainActivity.kt
@@ -38,55 +38,6 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    // Show the add recipe dialog
-    private fun showAddRecipeDialog(recipe: Recipe?) {
-        val dialogBinding = ActivityAddEditBinding.inflate(layoutInflater)
-
-        val categories = listOf("Breakfast", "Lunch", "Dinner", "Dessert", "Other")
-        val index = categories.indexOf(it.category)
-        if (index >= 0) dialogBinding.categorySpinner.setSelection(index)
-
-        recipe?.let {
-            dialogBinding.recipeTitleEditText.setText(it.title)
-            dialogBinding.categorySpinner.setText(it.category)
-            dialogBinding.descriptionEditText.setText(it.description)
-            dialogBinding.ingredientsEditText.setText(it.ingredients)
-            dialogBinding.instructionsEditText.setText(it.instructions)
-        }
-        val dialog = AlertDialog.Builder(this)
-            .setView(dialogBinding.root)
-            .create()
-
-
-        dialogBinding.saveButton.setOnClickListener {
-            val title = dialogBinding.recipeTitleEditText.text.toString()
-            val category = dialogBinding.categorySpinner.selectedItem.toString()
-            val description = dialogBinding.descriptionEditText.text.toString()
-            val ingredients = dialogBinding.ingredientsEditText.text.toString()
-            val instructions = dialogBinding.instructionsEditText.text.toString()
-            if (title.isNotEmpty() &&
-                category.isNotEmpty() &&
-                description.isNotEmpty() &&
-                ingredients.isNotEmpty() &&
-                instructions.isNotEmpty()
-            ) {
-                if (recipe == null) {
-                    viewModel.addRecipe(title, category, description, ingredients, instructions)
-                } else {
-                    val updatedRecipe = recipe.copy(
-                        title = title,
-                        category = category,
-                        description = description,
-                        ingredients = ingredients,
-                        instructions = instructions
-                    )
-                    viewModel.editRecipe(updatedRecipe)
-                }
-                dialog.dismiss()
-            }
-        }
-        dialog.show()
-    }
 
     private fun showDeleteDialog(recipe: Recipe) {
         val dialogBinding = DialogConfirmDeleteBinding.inflate(layoutInflater)
