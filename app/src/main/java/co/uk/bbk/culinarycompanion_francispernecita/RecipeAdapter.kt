fc56ba9fd70ee8f4a228ebde03f8047d1e6c44ab
Francis Pernecita <fperne01@student.bbk.ac.uk>
2025-06-25 10:46:01 +0100

    Implement RecipeAdapter with ViewHolder and imports for data binding

--- app/src/main/java/co/uk/bbk/culinarycompanion_francispernecita/RecipeAdapter.kt
+++ app/src/main/java/co/uk/bbk/culinarycompanion_francispernecita/RecipeAdapter.kt
@@ -1,4 +1,9 @@
 package co.uk.bbk.culinarycompanion_francispernecita
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import co.uk.bbk.culinarycompanion_francispernecita.databinding.RecipeItemBinding
+
 class RecipeAdapter {
 }
