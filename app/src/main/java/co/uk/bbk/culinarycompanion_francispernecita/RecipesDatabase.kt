3110dd0248bf56f82f0a7aaf7ced702c0b4574dd
Francis Pernecita <fperne01@student.bbk.ac.uk>
2025-06-25 11:17:21 +0100

    Create DAO and companion object for RecipesDatabase based on the week 8 lectuere

--- app/src/main/java/co/uk/bbk/culinarycompanion_francispernecita/RecipesDatabase.kt
+++ app/src/main/java/co/uk/bbk/culinarycompanion_francispernecita/RecipesDatabase.kt
@@ -4,7 +4,13 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-
+// code reused from the week 8 lecture
 @Database(entities = [Recipe::class], version = 1)
 abstract class RecipesDatabase : RoomDatabase() {
+    abstract fun recipesDao(): RecipesDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: RecipesDatabse? = null
+    }
 }
