587a129d32178545e17a15b788d83d0075a97e52
Francis Pernecita <fperne01@student.bbk.ac.uk>
2025-06-25 11:34:32 +0100

    Refactor: Remove unused imports and add necessary imports in MainActivity

--- app/src/main/java/co/uk/bbk/culinarycompanion_francispernecita/MainActivity.kt
+++ app/src/main/java/co/uk/bbk/culinarycompanion_francispernecita/MainActivity.kt
@@ -3,8 +3,12 @@ package co.uk.bbk.culinarycompanion_francispernecita
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
+import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
+import co.uk.bbk.culinarycompanion_francispernecita.R
+import co.uk.bbk.culinarycompanion_francispernecita.databinding.ActivityMainBinding
+import co.uk.bbk.culinarycompanion_francispernecita.databinding.RecipesDialogBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
