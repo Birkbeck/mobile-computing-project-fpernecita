fd501497a46378d359f28a54b50bad4fca6d8c72
Francis Pernecita <fperne01@student.bbk.ac.uk>
2025-06-27 15:03:40 +0100

    Implement RecipeItemDiffCallback for RecyclerView

--- app/src/main/java/co/uk/bbk/culinarycompanion_francispernecita/RecipeItemDiffCallback.kt
+++ app/src/main/java/co/uk/bbk/culinarycompanion_francispernecita/RecipeItemDiffCallback.kt
@@ -3,5 +3,11 @@ package co.uk.bbk.culinarycompanion_francispernecita
 import androidx.recyclerview.widget.DiffUtil
 
 class RecipeItemDiffCallback {
+    fun areItemsTheSame(oldItem: RecipeListItem, newItem: RecipeListItem): Boolean {
+        return oldItem == newItem
+    }
+    fun areContentsTheSame(oldItem: RecipeListItem, newItem: RecipeListItem): Boolean {
+        return oldItem == newItem
+    }
 
 }
