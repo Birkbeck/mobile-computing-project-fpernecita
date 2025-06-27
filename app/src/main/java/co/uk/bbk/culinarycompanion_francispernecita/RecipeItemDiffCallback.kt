971a760e335c553abf466ff406a449cfd248f41b
Francis Pernecita <fperne01@student.bbk.ac.uk>
2025-06-27 15:04:03 +0100

    Fix RecyclerView not updating by implementing DiffUtil.ItemCallback

--- app/src/main/java/co/uk/bbk/culinarycompanion_francispernecita/RecipeItemDiffCallback.kt
+++ app/src/main/java/co/uk/bbk/culinarycompanion_francispernecita/RecipeItemDiffCallback.kt
@@ -2,7 +2,7 @@ package co.uk.bbk.culinarycompanion_francispernecita
 
 import androidx.recyclerview.widget.DiffUtil
 
-class RecipeItemDiffCallback {
+class RecipeItemDiffCallback : DiffUtil.ItemCallback<RecipeListItem>() {
     fun areItemsTheSame(oldItem: RecipeListItem, newItem: RecipeListItem): Boolean {
         return oldItem == newItem
     }
