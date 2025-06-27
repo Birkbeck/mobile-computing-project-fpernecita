0e9f87c7906f6bc678d7bf755d6ef1910ee47cb4
Francis Pernecita <fperne01@student.bbk.ac.uk>
2025-06-27 15:05:25 +0100

    Add override keyword to RecipeItemDiffCallback methods

--- app/src/main/java/co/uk/bbk/culinarycompanion_francispernecita/RecipeItemDiffCallback.kt
+++ app/src/main/java/co/uk/bbk/culinarycompanion_francispernecita/RecipeItemDiffCallback.kt
@@ -3,11 +3,10 @@ package co.uk.bbk.culinarycompanion_francispernecita
 import androidx.recyclerview.widget.DiffUtil
 
 class RecipeItemDiffCallback : DiffUtil.ItemCallback<RecipeListItem>() {
-    fun areItemsTheSame(oldItem: RecipeListItem, newItem: RecipeListItem): Boolean {
+    override fun areItemsTheSame(oldItem: RecipeListItem, newItem: RecipeListItem): Boolean {
         return oldItem == newItem
     }
-    fun areContentsTheSame(oldItem: RecipeListItem, newItem: RecipeListItem): Boolean {
+    override fun areContentsTheSame(oldItem: RecipeListItem, newItem: RecipeListItem): Boolean {
         return oldItem == newItem
     }
-
 }
