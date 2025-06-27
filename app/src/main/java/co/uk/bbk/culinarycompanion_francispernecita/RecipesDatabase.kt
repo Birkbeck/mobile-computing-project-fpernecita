1569bc33d80fc92c62525476bff393fceddb475c
Francis Pernecita <fperne01@student.bbk.ac.uk>
2025-06-27 17:19:19 +0100

    Address exportSchema warning in RecipesDatabase

--- app/src/main/java/co/uk/bbk/culinarycompanion_francispernecita/RecipesDatabase.kt
+++ app/src/main/java/co/uk/bbk/culinarycompanion_francispernecita/RecipesDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 // code reused from the week 8 lecture
-@Database(entities = [Recipe::class], version = 1)
+@Database(entities = [Recipe::class], version = 1, exportSchema = false)
 abstract class RecipesDatabase : RoomDatabase() {
     abstract fun recipesDao(): RecipesDao
 
