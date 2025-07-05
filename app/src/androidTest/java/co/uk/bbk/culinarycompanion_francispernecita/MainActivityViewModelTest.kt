1baa4170f002be97ec04a8ae5350a01c131e7f08
Francis Pernecita <fperne01@student.bbk.ac.uk>
2025-07-06 00:41:31 +0100

    Create an empty test class for MainActivityViewModel

---
+++ app/src/androidTest/java/co/uk/bbk/culinarycompanion_francispernecita/MainActivityViewModelTest.kt
@@ -0,0 +1,11 @@
+package co.uk.bbk.culinarycompanion_francispernecita
+
+import android.content.Context
+import androidx.lifecycle.ViewModelProvider
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+class MainActivityViewModelTest {
+
+}
