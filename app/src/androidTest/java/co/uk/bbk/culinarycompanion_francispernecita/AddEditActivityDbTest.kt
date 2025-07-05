c898c2cdbd09fe9be8103cda6710d6a5322735df
Francis Pernecita <fperne01@student.bbk.ac.uk>
2025-07-06 00:17:24 +0100

    Correct casing of AddEditActivityDbTest filename

---
+++ app/src/androidTest/java/co/uk/bbk/culinarycompanion_francispernecita/AddEditActivityDbTest.kt
@@ -0,0 +1,12 @@
+package co.uk.bbk.culinarycompanion_francispernecita
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+// JUnit
+import org.junit.runner.RunWith
+// Coroutines, Room
+// App classes
+
+@RunWith(AndroidJUnit4::class)
+class AddEditActivityDbTest {
+
+}
