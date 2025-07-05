d9b264e601caa73470deca6147abca2b7db7006d
Francis Pernecita <fperne01@student.bbk.ac.uk>
2025-07-06 00:42:18 +0100

    Add missing imports to MainActivityViewModelTest

--- app/src/androidTest/java/co/uk/bbk/culinarycompanion_francispernecita/MainActivityViewModelTest.kt
+++ app/src/androidTest/java/co/uk/bbk/culinarycompanion_francispernecita/MainActivityViewModelTest.kt
@@ -5,6 +5,12 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+// JUnit
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.Assert.assertTrue
 
 class MainActivityViewModelTest {
 
