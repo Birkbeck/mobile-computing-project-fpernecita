c3c3aa09e482612d20c2cdbde1e99b7085968159
Francis Pernecita <fperne01@student.bbk.ac.uk>
2025-07-06 00:18:29 +0100

    Add dependency imports for AddEditDbTest.kt

--- app/src/androidTest/java/co/uk/bbk/culinarycompanion_francispernecita/AddEditActivityDbTest.kt
+++ app/src/androidTest/java/co/uk/bbk/culinarycompanion_francispernecita/AddEditActivityDbTest.kt
@@ -1,6 +1,10 @@
 package co.uk.bbk.culinarycompanion_francispernecita
 
+import android.content.Context
+import androidx.lifecycle.ViewModelProvider
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.core.app.ActivityScenario
 // JUnit
 import org.junit.runner.RunWith
 // Coroutines, Room
