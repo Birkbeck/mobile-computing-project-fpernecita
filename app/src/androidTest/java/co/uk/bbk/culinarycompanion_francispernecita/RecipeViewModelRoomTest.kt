15dc2dba92dd95c7f8f9af5b14cfd465732fde60
Francis Pernecita <fperne01@student.bbk.ac.uk>
2025-07-06 01:27:20 +0100

    Add Coroutines test dependencies to RecipeViewModelRoomTest

--- app/src/androidTest/java/co/uk/bbk/culinarycompanion_francispernecita/RecipeViewModelRoomTest.kt
+++ app/src/androidTest/java/co/uk/bbk/culinarycompanion_francispernecita/RecipeViewModelRoomTest.kt
@@ -8,7 +8,13 @@ import androidx.room.Room
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
-
+// courtines
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.setMain
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
 
 class RecipeViewModelRoomTest {
 
