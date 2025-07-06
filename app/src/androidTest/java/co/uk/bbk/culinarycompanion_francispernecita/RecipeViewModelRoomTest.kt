e4f43def1d6ceb89a21512d6d3dc2bd3277fabf8
Francis Pernecita <fperne01@student.bbk.ac.uk>
2025-07-06 01:26:19 +0100

    Add dependency imports for lifecycle testing and live data in RecipeViewModelTest

--- app/src/androidTest/java/co/uk/bbk/culinarycompanion_francispernecita/RecipeViewModelRoomTest.kt
+++ app/src/androidTest/java/co/uk/bbk/culinarycompanion_francispernecita/RecipeViewModelRoomTest.kt
@@ -4,6 +4,10 @@ import android.content.Context
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.room.Room
+// lifecycle testing
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
 
 class RecipeViewModelRoomTest {
 
